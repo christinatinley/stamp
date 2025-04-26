f5e630dac09170086554e0e0b7c6bc3f0bd2ba0c
ebelashchenko <ebelashchenko2@huskers.unl.edu>
2025-04-26 14:37:59 -0500

    copied model from practice rag model

--- server/knowledge_base/model.py
+++ server/knowledge_base/model.py
@@ -17,3 +17,19 @@ def build_itinerary(city_name):
     places = knowledge_base.get_places(lat, lng)
     for p in places:
         add_chunk_to_database(p)
+
+def cosine_similarity(a, b):
+  dot_product = sum([x * y for x, y in zip(a, b)])
+  norm_a = sum([x ** 2 for x in a]) ** 0.5
+  norm_b = sum([x ** 2 for x in b]) ** 0.5
+  return dot_product / (norm_a * norm_b)
+
+def retrieve(query, top_n=3):
+  query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)['embeddings'][0] 
+  # temporary list to store (chunk, similarity) pairs
+  similarities = []
+  for chunk, embedding in VECTOR_DB:
+    similarity = (chunk, cosine_similarity(query_embedding, embedding))
+    similarities.append(similarity)
+  similarities.sort(key=lambda x: x[1], reverse=True)
+  return similarities[:top_n]
