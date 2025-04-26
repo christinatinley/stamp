e3e712a54ea256603b851570494ca99b7b873a93
christinatinley <ctinley04@gmail.com>
2025-04-26 13:41:20 -0500

    Deletes flasgger

--- server/app/app.py
+++ server/app/app.py
@@ -1,24 +1,21 @@
 from flask import Flask, request, jsonify
 import requests
-from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
-swagger = Swagger(app)
 
 
 @app.route('/places', methods=['GET'])
-@swag_from('app.yml')
 def get_places():
     input_text = request.args.get('input')
     api_key = "AIzaSyCjZ_ClVlhmfOTrzkbjpjk8LV_UHD68zgk"
 
-    url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json"
+    url = f"https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=pizza%20near%20par&key=AIzaSyCjZ_ClVlhmfOTrzkbjpjk8LV_UHD68zgk"
     params = {
         "input": input_text,
         "key": api_key
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get(url)
     return jsonify(response.json())
 
 
