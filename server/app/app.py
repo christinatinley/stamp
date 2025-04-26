f958ca2f6da4bc975780d3563a067832963b63d7
ebelashchenko <ebelashchenko2@huskers.unl.edu>
2025-04-26 13:58:44 -0500

    initial backend setup with .env file and proper folders set up

--- server/app/app.py
+++ server/app/app.py
@@ -6,21 +6,5 @@ app = Flask(__name__)
 swagger = Swagger(app)
 
 
-@app.route('/places', methods=['GET'])
-@swag_from('app.yml')
-def get_places():
-    input_text = request.args.get('input')
-    api_key = "AIzaSyCjZ_ClVlhmfOTrzkbjpjk8LV_UHD68zgk"
-
-    url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json"
-    params = {
-        "input": input_text,
-        "key": api_key
-    }
-
-    response = requests.get(url, params=params)
-    return jsonify(response.json())
-
-
 if __name__ == "__main__":
     app.run(debug=True)
