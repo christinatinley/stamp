417aa01ce10e16d755d0d41decd79db2ae5c5908
christinatinley <ctinley04@gmail.com>
2025-04-28 16:30:05 -0500

    Cleans up unused files

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,4 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import QuizFormPage from './pages/QuizFormPage.vue'
 
 export default {
@@ -16,30 +14,5 @@ export default {
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script>
+import { Menubar } from 'primevue/menubar';
+</script>
+
+<template>
+<Menubar :model="items" >
+
+</Menubar>
+</template>
