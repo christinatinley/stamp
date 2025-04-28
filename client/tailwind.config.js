ff89b742c37d40fe431cbda817e666d727719868
christinatinley <ctinley04@gmail.com>
2025-04-28 16:29:53 -0500

    Sets up global css stuff

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#6D213C',
+        text: '#F9F0DC',
+        link: '#007bff',
+        'header-1': '#F9F0DC',
+        'header-2': '#CB769E',
+        'header-3': '#CB769E',
+        accent: '#226D5E',
+        'accent-2': '#DDE1FF',
+      },
+      fontFamily: {
+        space: ['"Space Mono"', 'monospace'],
+        abril: ['"Abril Fatface"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
